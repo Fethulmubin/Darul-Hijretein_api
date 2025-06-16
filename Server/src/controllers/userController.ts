5d0e565e3ba1f5819c525a433436bd37bc4483c2
fethulmubin <fetihul@gmail.com>
2025-06-16 14:41:10 +0300

    validator added successfully for register and login

--- Server/src/controllers/userController.ts
+++ Server/src/controllers/userController.ts
@@ -5,27 +5,10 @@ import validator from 'validator';
 
 
 const Signup = async (req, res) => {
-    const strongPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&])[A-Za-z\d@$!%*?#&]{8,}$/
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
     //validating the request body
     const { name, email, password, confirmPassword } = req.body;
-    if (!name || !email || !password || !confirmPassword) {
-        return res.status(400).json({ status: false, message: 'All fields are required' });
-    }
-   //validating email format
-   if (!validator.isEmail(email)) {
-    return res.status(400).json({ message: 'Invalid email address' });
-  }
-
-    if (password !== confirmPassword) {
-        return res.status(400).json({ status: false, message: 'Passwords do not match' });
-    }
-    if (!strongPassword.test(password)) {
-        return res.status(400).json({
-            message:
-                'Password must be at least 8 characters long and include uppercase, lowercase, number, and special character.',
-        });
-    }
+    
     //registering user
     try {
         const existingUser = await prisma.user.findUnique({
@@ -59,9 +42,9 @@ const Signup = async (req, res) => {
 
 const Login = async (req, res) => {
     const { email, password } = req.body;
-    if (!email || !password) {
-        return res.status(400).json({ status: false, message: 'Email and password are required' });
-    }
+    // if (!email || !password) {
+    //     return res.status(400).json({ status: false, message: 'Email and password are required' });
+    // }
     try {
         const existingUser = await prisma.user.findUnique({
             where: {
